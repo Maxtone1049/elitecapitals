3c81864445c478b5999e225ceb0aa364c98f5814
Toni <tonymax1049@gmail.com>
2022-12-05 11:55:40 +0100

    Fresh

--- register.php
+++ register.php
@@ -471,7 +471,19 @@ if (isset($_POST['register'])) {
 		<i class="icofont icofont-simple-up"></i>
 	</div>
 	<!--back to top end-->
-
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/638dcc80b0d6371309d2ad73/1gjgudlsb';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 	<!--jquery script load-->
 	<script src="auth-assets/front/js/jquery.js"></script>
 	<!--Owl carousel script load-->
--- trade/refer.php
+++ trade/refer.php
@@ -343,5 +343,17 @@ e.preventDefault();
     <script src="js/sb-admin-2.min.js"></script>
 
 </body>
-
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/638dcc80b0d6371309d2ad73/1gjgudlsb';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 </html>
